eabb16fc57997f8b6acfec3ca9896c568913e016
Shuma Shinozaki <shinozaki@cloud-ace.jp>
2022-05-01 14:23:00 +0900

    不要なファイル削除

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,13 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PassportModule } from '@nestjs/passport';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { Auth0Service } from './auth0.service';
 import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [ConfigModule.forRoot(), PassportModule],
   controllers: [AppController],
-  providers: [AppService, Auth0Service, JwtStrategy],
+  providers: [Auth0Service, JwtStrategy],
 })
 export class AppModule {}
