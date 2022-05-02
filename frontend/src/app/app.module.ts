6883f65d02ecea8ecdb29b7a1055d5376178f964
Shuma Shinozaki <shinozaki@cloud-ace.jp>
2022-05-02 21:36:24 +0900

    ストア実装

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,8 +6,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { AuthModule } from '@auth0/auth0-angular';
 
+import * as User from './user/user.reducer';
 import { JwtInterceptor } from './jwt.interceptor';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,6 +28,10 @@ import { environment } from '../environments/environment';
     MatListModule,
     MatSidenavModule,
     MatToolbarModule,
+    StoreModule.forRoot({
+      [User.usersFeatureKey]: User.reducer,
+    }),
+    StoreDevtoolsModule.instrument(),
     AuthModule.forRoot({
       domain: environment.domain,
       clientId: environment.clientId,
