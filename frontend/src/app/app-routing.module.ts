67003c3429c9985447306fe4cfc48304bb2e0f13
Shuma Shinozaki <shinozaki@cloud-ace.jp>
2022-05-03 11:56:12 +0900

    フロント実装

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '@auth0/auth0-angular';
 
 import { UsersPageComponent } from './users-page/users-page.component';
 import { OrganizationsPageComponent } from './organizations-page/organizations-page.component';
@@ -8,10 +9,17 @@ const routes: Routes = [
   {
     path: 'users',
     component: UsersPageComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'organizations',
     component: OrganizationsPageComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: '',
+    redirectTo: 'users',
+    pathMatch: 'full',
   },
 ];
 
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(private auth: AuthService) {}
+
+  ngOnInit() {
+    this.auth.isAuthenticated$.subscribe(
+      (isAuth) => !isAuth && this.auth.loginWithRedirect()
+    );
+  }
+}
